d6a41c8956457e43e7e3d8853af86dfeb2022e4b
Guillemj98 <guillemagro98@gmail.com>
2024-04-11 13:08:41 +0200

    Documentación de clases, atributos y metodos de las entidades jugador y soldado

--- Actividad14ED/src/Entidades/Jugador.java
+++ Actividad14ED/src/Entidades/Jugador.java
@@ -1,51 +1,82 @@
 package Entidades;
-
+/**
+ * Esta clase define objetos (jugador) que contienen 3 atributos
+ * (dorsal, numeroTarjetasAmarillas y numeroTarjetasRojas)
+ * y 2 métodos: 
+ * <ul>
+ *  <li>Imprime el numero del dorsal del objeto jugador</li>
+ *  <li>Devuelve si esta expulsado o no</li>
+ *  </ul>
+ * @author Guillermito
+ * @version 17.0
+ * 
+ */
 public class Jugador {
-	 private int dorsal;
+	/**
+	 * Atributo que determina el numero del dorsal del jugador
+	 */
+	private int dorsal;
+	/**
+	 * Atributo que indica el numero de tarjetas amarillas
+	 */
+	private int numeroTarjetasAmarillas;
+	/**
+	 * Atributo que indica el numero de tarjetas rojas
+	 */
+	private int numeroTarjetasRojas;
+	
+	public int getDorsal() {
+		return dorsal;
+	}
 
-     private int numeroTarjetasAmarillas;
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
 
-     private int numeroTarjetasRojas;
+	public int getNumeroTarjetasAmarillas() {
+		return numeroTarjetasAmarillas;
+	}
 
-                  
+	public void setNumeroTarjetasAmarillas(int numeroTarjetasAmarillas) {
+		this.numeroTarjetasAmarillas = numeroTarjetasAmarillas;
+	}
 
-//Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
+	public int getNumeroTarjetasRojas() {
+		return numeroTarjetasRojas;
+	}
 
+	public void setNumeroTarjetasRojas(int numeroTarjetasRojas) {
+		this.numeroTarjetasRojas = numeroTarjetasRojas;
+	}
 
-
-     public void ponerDorsal(int dorsal) {
-
-                   if(dorsal >= 1 && dorsal <= 30) {
-
-                                 this.dorsal = dorsal;
-
-                   }else {
-
-                                 this.dorsal = -1;
-
-                   }
-
-     }
-
-    
-
-     public boolean estaExpulsado() {
-
-                   boolean expulsado = false;                                                  
-
-                   if(numeroTarjetasAmarillas == 2) {
-
-                                 expulsado = true;
-
-                   }                          
-
-                   if(numeroTarjetasRojas == 1) {
-
-                                 expulsado = true;
-
-                   }                          
-
-                   return expulsado;
-
-     }            
+	/**
+	 * Método que establece el <b>dorsal</b> del jugador comprendido entre los valores
+	 * <b>1 y 30 (incluidos)</b> y en caso contrario lo establece a (-1)
+	 * @param dorsal numero que introducimos por parámetro
+	 */
+	public void ponerDorsal(int dorsal) {
+		if (dorsal >= 1 && dorsal <= 30) {
+			this.dorsal = dorsal;
+		} else {
+			this.dorsal = -1;
+		}
+	}
+	/**
+	 * Método que devuelve si el jugador esta expulsado o no.
+	 * Determina si el jugador esta expulsado, si el número de 
+	 * amarillas es igual a 2 o el numero de rojas es igual 1, 
+	 * en caso contrario devuelve false
+	 * @return devuelve false si el jugador no tiene 2 amarillas o una tarjeta roja,
+	 * en caso contrario devuelve true.
+	 */
+	public boolean estaExpulsado() {
+		boolean expulsado = false;
+		if (numeroTarjetasAmarillas == 2) {
+			expulsado = true;
+		}
+		if (numeroTarjetasRojas == 1) {
+			expulsado = true;
+		}
+		return expulsado;
+	}
 }
--- Actividad14ED/src/Entidades/Soldado.java
+++ Actividad14ED/src/Entidades/Soldado.java
@@ -1,35 +1,73 @@
 package Entidades;
+/**
+ * Esta clase define objetos (soldado), el cual contiene 2 atributos
+ *(estaMuerto y numeroBalas)
+ * Tambien tiene 2 métodos :
+ * <ul>
+ * 	<li> Devuelve si puede disparar o no</li>
+ * 	<li> Reduce el numero de balas y establece estaMuerto a false de un soldado 
+ * 	pasado por parametro</li>
+ * </ul>
+ * @author Guillermito
+ * @version 17.0
+ */
+public class Soldado {	
+	/**
+	 * Atributo de tipo boolean el cual determina si 
+	 * el objeto esta vivo o muerto
+	 */
+	private boolean estaMuerto;
+	/**
+	 * Atributo que determina el numero de balas
+	 * que tiene el objeto soldado.
+	 */
+	private int numeroBalas;
+	
+	
+	
+	public boolean isEstaMuerto() {
+		return estaMuerto;
+	}
 
-public class Soldado {
-	  private boolean estaMuerto;
+	public void setEstaMuerto(boolean estaMuerto) {
+		this.estaMuerto = estaMuerto;
+	}
 
-      private int numeroBalas;
+	public int getNumeroBalas() {
+		return numeroBalas;
+	}
 
-     
+	public void setNumeroBalas(int numeroBalas) {
+		this.numeroBalas = numeroBalas;
+	}
 
-      //Crear los métodos “get” y “set” de los atributos cuando se vayan a hacer las pruebas y la documentación. Aquí no se han creado porque no aportan nada
+	/**
+	 * Método que devuelve si el objeto puede disparar o no.
+	 * El método devolvera true si el numero de balas es superior a 0
+	 * en caso contrario devolvera false
+	 * @return Devolvera true si el numero de balas es mayor que 0
+	 * y por el contrario devolvera false si es menor o igual a 0.
+	 */
+	public boolean puedeDisparar() {
 
-     
+		if (this.numeroBalas > 0) {
 
-      public boolean puedeDisparar() {
+			return true;
+		}
 
-                    if(this.numeroBalas > 0) {
+		return false;
+	}
+	/**
+	 * Método que permite disparar al objeto.
+	 * Disminuira el numero de balas y establecerá
+	 * el atributo estaMuerto a true del objeto pasado por 
+	 * parámetro
+	 * @param sol Objeto pasado por parámetro
+	 */
+	public void disparar(Soldado sol) {
 
-                                  return true;
+		this.numeroBalas--;
 
-                    }                          
-
-                    return false;
-
-      }
-
-     
-
-      public void disparar(Soldado sol) {
-
-                    this.numeroBalas--;
-
-                    sol.estaMuerto = true;
-
-      }
+		sol.estaMuerto = true;
+	}
 }
